6ce8f348664c84e3ae9a787518bacac6df5482b4
David Petersen <dpete106@gmail.com>
2015-12-11 12:00:54 -0500

    PHP5  OOP PDO webcam.php
    fix tweet egret.tv

--- hero/webcam.php
+++ hero/webcam.php
@@ -20,7 +20,7 @@ include('header.php');
 		<div class="col-md-6">
 		<h3>tweet when they eat!</h3>
 		<p>
-<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://www.egrettv.org/hero/webcam.php" data-via="storkman" data-lang="en" data-related="anywhereTheJavascriptAPI" data-count="vertical">Tweet</a>
+<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://www.egret.tv/hero/webcam.php" data-via="storkman" data-lang="en" data-related="anywhereTheJavascriptAPI" data-count="vertical">Tweet</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>		
 	</p>
 		</div>
