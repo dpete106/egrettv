a5f40eaa50d5daf41057b91a727588c5e55d9737
David Petersen <dpete106@gmail.com>
2016-01-13 14:58:48 -0500

    PHP5 OOP PDO webcam
    webcam.php

--- hero/webcam.php
+++ hero/webcam.php
@@ -15,7 +15,11 @@ include('header.php');
 		<p>Welcome to the egret.tv webcam streaming live on the Long Island Sound.</p>
 		<p>During the long summer season you will see egrets and herons walking about and feeding at this bird habitat in Branford, Connecticut.</p>
 		<p>The best times to view them are low-tides in the early mornings and low-tides in the early evenings.</p>
-				<object width="480" height="324"><param name="movie" value="https://www.dropcam.com/e/738a41b1ea8446508cdc9e080302d962?autoplay=false"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><param name="wmode" value="opaque"></param><embed src="https://www.dropcam.com/e/738a41b1ea8446508cdc9e080302d962?autoplay=false" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="480" height="324" wmode="opaque"></embed></object>
+ <iframe type="text/html" frameborder="0" width="480" height="394" src="//video.nest.com/embedded/live/sPIEVp?autoplay=1"></iframe>
+				<!-- <object width="480" height="324"><param name="movie" value="https://www.dropcam.com/e/738a41b1ea8446508cdc9e080302d962?autoplay=false"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><param name="wmode" value="opaque"></param><embed src="https://www.dropcam.com/e/738a41b1ea8446508cdc9e080302d962?autoplay=false" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="480" height="324" wmode="opaque"></embed></object> -->
+
+
+
 		</div>
 		<div class="col-md-6">
 		<h3>tweet when they eat!</h3>
@@ -23,6 +27,19 @@ include('header.php');
 <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://www.egret.tv/hero/webcam.php" data-via="storkman" data-lang="en" data-related="anywhereTheJavascriptAPI" data-count="vertical">Tweet</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>		
 	</p>
+	<div>
+	<form action="" method="POST">
+  <script
+    src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+    data-key="pk_test_JRGK5txjD4IIgyJU5ztDwSz1"
+    data-amount="2000"
+    data-name="egret.TV"
+    data-description="2 widgets ($20.00)"
+    data-image="/images/egret_logo.png"
+    data-locale="auto">
+  </script>
+</form>
+	</div>
 		</div>
 	</div>
 
