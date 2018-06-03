1179c1883502dfaa58b29c6172bdf83b7b05286e
David Petersen <dpete106@gmail.com>
2018-06-03 10:16:40 -0400

    startup front-end

--- index.php
+++ index.php
@@ -10,63 +10,81 @@ include('./hero/header_test.php');
 
 
     <!-- Main jumbotron for a primary marketing message or call to action -->
-
-    <div class="jumbotron"><div class="module">
-	    <div class="col-lg-12">
+<main role="main">
+    <div style="margin-left: 125px; margin-right: 125px;" class="jumbotron">
+		<div class="container">
+			<h1>egret.tv | Help protect the Long Island Sound</h1>
+			<hr class="border-row-heavy">
+			<div class="row">
+				<div class="col-lg-12">
 		
-		<img src="/egrettv/images/egret_logo.png"  class="img-responsive"  style="position: relative;width: 100%;height: auto;">
-		<p>
-		<a href="https://twitter.com/storkman" class="twitter-follow-button" data-show-count="false">Follow @storkman</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-		</p>
-		<div align="center" class="embed-responsive embed-responsive-16by9">
-		<video autoplay loop class="embed-responsive-item">
-        <source src="/egrettv/images/giphy.mp4" type="video/mp4">
-		</video>
-		</div>
-		
-		</div><br>
-      
+				<div align="center" class="embed-responsive embed-responsive-16by9">
+				<video autoplay loop class="embed-responsive-item" controls>
+				<source src="/egrettv/images/giphy.mp4" type="video/mp4" >
+				</video>
+				</div>
+				</div>
+			</div>      
         
-        <p class="lead"><em>If we don't get to know them, we're gonna lose them</em></p>
-		<p class="lead"><em>Support egret.tv - Watch the streaming webcam of the Long Island Sound!</em></p>
-        <p><a class="btn btn-primary btn-lg" href="/egrettv/hero/webcam.php" role="button">Learn more &raquo;</a></p>
-        
+			<p class="lead"><em>If we don't get to know them, we're going to lose them.</em></p>
+			<p>
+			<button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+			Read More
+				</button>
+			</p>
+			
+			<div class="collapse" id="collapseExample">
+			<div class="card card-body">
+             <p>Birds are sensitive indicators of the environment, a sort of "ecological litmus paper".  The observations of bird populations over time leads to environmental awareness and a signal of possible change - Roger Tory Peterson. </p>
+			</div>
+			</div>
+		</div> <!-- /container -->
 
-    </div> <!-- /module -->
+      <!-- Example row of columns -->
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+				<h4>Mission</h4>
+				<p>The mission of egret.tv is to the protect and preserve the Long Island Sound ecosystem.
+				The vision of egret.tv is a sustainable healthy Long Island Sound coexisting with a vibrant economy benefiting all people living in the region.
+				The work of egret.tv supports clean air, water and habitat of the Long Island Sound, slowing climate change, and over-development protection. 
+				egret.tv is a tax exempt organization under Section 501(c)3 of the Internal Revenue Code (pending status).
+				</p>
+				</p>
+				<img src="/egrettv/images/ltern.gif"  class="img-fluid"  >
+				</div>
+				
+				<div class="col-lg-6">
+				<h4>Advocacy</h4>
+				<p>egret.tv participates in the real-world and social media community of environmentalist and environmental organizations in order to help raise awareness of the issues that are detrimental to the health of the Long Island Sound.
+				Its goal is promote safe citizen behaviors which lead to the sustainability of the Long Island Sound ecosystem.
+				egret.tv supports laws and the political process which protect our natural resources.</p>
+				<img src="/egrettv/images/gegret.gif"  class="img-fluid"  >
+				</div>
+			</div>
+		
+			<div class="row">
+				<div class="col-lg-6">
+				<h4>Action</h4>
+				<p>egret.tv hosts a streaming webcam of the Long Island Sound.  In social media egret.tv uses its publishing platform, content distribution, and social networking links as an integral part of the environmentalist community.</p>
+          
+				<img src="/egrettv/images/snegret.gif"  class="img-fluid"  >
+				</div>
+				<div class="col-lg-6">
+				</div>
+			</div>
+		
+		</div> <!-- /container -->
 
 	</div> <!-- /jumbotron -->
-      <!-- Example row of columns -->
-      <div class="row marketing">
-        <div class="col-lg-6">
-          <h4>Mission</h4>
-          <p>Birds are sensitive indicators of the environment, a sort of "ecological litmus paper".  The observations of bird populations over time leads to environmental awareness and a signal of possible change. egret.tv is a social media website dedicated to the protection, restoration and preservation of Connecticut's Long Island Sound ecosystem.  Its goal is to be a site for environmental activists advocating public policy and individual behavior sustaining the Long Island Sound ecosystem.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-          <img src="/egrettv/images/ltern.gif"  class="img-responsive"  >
-        </div>
-        <div class="col-lg-6">
-          <h4>Advocacy</h4>
-          <p>egret.tv participates in the community of environmentalist and environmental organizations in order to help raise awareness of the issues that are detrimental to the health of the Long Island Sound.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-          <img src="/egrettv/images/gegret.gif"  class="img-responsive"  >
-       </div>
-        <div class="col-lg-6">
-          <h4>Action</h4>
-          <p>egret.tv hosts a streaming webcam of the Long Island Sound.  In social media egret.tv uses its publishing platform, content distribution, and social networking links as an integral part of the environmentalist community.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-          
-          <img src="/egrettv/images/snegret.gif"  class="img-responsive"  >
-        </div>
-      </div>
 
-      <hr>
-
+</main>
       <footer>
         <?php
 include('./hero/footer.php');
 ?>
       </footer>
 
-    </div> <!-- /container -->
 
 
     <!-- Bootstrap core JavaScript
