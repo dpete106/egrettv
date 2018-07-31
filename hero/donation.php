5c19f67b8ee1b62416783f6f8f6c8c9d7d541bec
David Petersen <dpete106@gmail.com>
2018-07-31 11:23:40 -0400

    2018 donation project

--- hero/donation.php
+++ hero/donation.php
@@ -52,6 +52,12 @@ $shipping_errors = array();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 					
+	//get donation number
+	$str = $_POST['amount'];
+	preg_match_all('!\d+!', $str, $matches);
+	$var = implode(' ', $matches[0]);
+	$order_total = $var . "00";
+
 	
 	// Check for Magic Quotes:
 	if (get_magic_quotes_gpc()) {
@@ -203,7 +209,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 			$cc_last_four = 1234;
 			$shipping = 0;
 			$customer_id = $_SESSION['customer_id'];
-			$order_total = 2500;
+			//$order_total = 2500;
 
 			$q1 = 'INSERT INTO orders (customer_id, total, shipping, credit_card_number, order_date) VALUES (?, ?, ?, ?, NOW())';
 			$affected = 0;
@@ -353,7 +359,7 @@ if (isset($message)) { // this type of error message is a system error
       </div>
 
       <div class="row">
-        <div class="col-md-4 order-md-2 mb-4">
+        <!-- <div class="col-md-4 order-md-2 mb-4">
           <h4 class="d-flex justify-content-between align-items-center mb-3">
             <span class="text-muted">Your cart</span>
             <span class="badge badge-secondary badge-pill">1</span>
@@ -372,12 +378,33 @@ if (isset($message)) { // this type of error message is a system error
             </li>
           </ul>
 
-        </div>
+        </div> -->
         <div class="col-md-8 order-md-1">
-          <h4 class="mb-3">Billing address</h4>
           <form class="needs-validation" novalidate action="/egrettv/hero/donation.php" method="POST" id="billing_form">
-            <div class="row">
-              <div class="col-md-6 mb-3">
+
+			<div class="row">
+              <div class="col-md-4 order-md-2 mb-4">
+				<h4 class="d-flex justify-content-between align-items-center mb-3">
+					<span class="text-muted">Your cart</span>
+					<span class="badge badge-secondary badge-pill">1</span>
+				</h4>
+                <label for="amount">Donation Amount</label>
+                <select class="form-control" name="amount" id="amount" required>
+                  <option value="">Choose...</option>
+                  <option>Total    $25</option>
+                  <option>Total    $50</option>
+                  <option>Total    $75</option>
+                  <option>Total    $100</option>
+              </select>
+                <div class="invalid-feedback">
+                  Please provide a valid amount.
+                </div>
+              </div>
+            </div>
+			
+          <h4 class="mb-3">Billing address</h4>
+             <div class="row">
+             <div class="col-md-6 mb-3">
                 <label for="firstName">First name</label>
                 <!--<input type="text" class="form-control" id="firstName" placeholder="" value="" required> -->
 				<?php create_form_input('firstName', 'text', $shipping_errors); ?>
@@ -487,12 +514,12 @@ if (isset($message)) { // this type of error message is a system error
 			</div>
 			
            <div class="row">
-             <div class="d-block my-3">
+				<div class="col-lg-12">
               <div class="custom-control custom-radio">
                 <input id="credit" name="paymentMethod" type="radio" class="custom-control-input" checked required>
                 <label class="custom-control-label" for="credit">Credit Card Payments</label>
               </div>
-			  <p>egret.tv does not store your credit card information in any way.  We use Stripe.com, one of the most secure and reputable payment processors available.</p>
+				<div class="alert alert-info" id="error_span">egret.tv does not store your credit card information in any way.  We use Stripe.com, one of the most secure and reputable payment processors available.</div>
               <!-- <div class="custom-control custom-radio">
                 <input id="debit" name="paymentMethod" type="radio" class="custom-control-input" required>
                 <label class="custom-control-label" for="debit">Debit card</label>
