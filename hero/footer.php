4390a77c6b22ff271f06990f206af0622e169643
David Petersen <dpete106@gmail.com>
2018-08-11 11:52:09 -0400

    2018 Donation project

--- hero/footer.php
+++ hero/footer.php
@@ -1,16 +1,16 @@
-<div style="background-color:rgba(192,192,192,0.1);" class="jumbotron">
-	<div class="container">
+<div id="footer">
+    <div class="container">
 		<div class="row">
 			<div class="col-lg-6">
-<p><a href="https://twitter.com/storkman" class="twitter-follow-button" data-show-count="false"><img src="/egrettv/images/twitter2.png" /></a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script></p>
+			<p><a href="https://twitter.com/storkman" class="twitter-follow-button" data-show-count="false"><img src="/egrettv/images/twitter2.png" /></a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script></p>
 			</div>
 			<div class="col-lg-6">
 <script src="https://apis.google.com/js/platform.js"></script>
-<div class="g-ytsubscribe" data-channelid="UC6BqMHGjvexrX0aTu_M4f9A" data-layout="default" data-count="default"></div>
+			<div class="g-ytsubscribe" data-channelid="UC6BqMHGjvexrX0aTu_M4f9A" data-layout="default" data-count="default"></div>
 			</div>
 		</div>
 		<div class="row">
-			<div class="list-group">
+			<div class="list-group"> 
 				<button type="button" class="list-group-item list-group-item-action" disabled>&copy; egret.tv 2018</button>
 				<button type="button" class="list-group-item list-group-item-action" data-toggle="modal" data-target="#exampleModal">
 				privacy policy</button>
@@ -44,5 +44,5 @@
 					</div>
 			</div>
 		</div>
-	</div>
-</div>
+	</div> <!-- container -->
+</div> <!-- footer -->
